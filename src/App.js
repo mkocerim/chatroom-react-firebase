9808363de49b93131370272818ed7526aa8d25c3
Mustafa Koçer <mkocerim@gmail.com>
2023-03-08 00:09:02 +0300

    chatroom with firabase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "../src/styles/style.css";
 
+import Auth from "./components/Auth";
+import Chat from "./components/Chat";
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("token"));
+  const [room, setRoom] = useState(null);
+  console.log(room);
+  const inputRef = useRef(null);
+  if (!isAuth) {
+    return (
+      <div className="container">
+        <Auth setIsAuth={setIsAuth} />
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {room ? (
+        <Chat />
+      ) : (
+        <div className="room-container">
+          <h1>Chat Odasi</h1>
+          <label>Hangi Odaya Gireceksiniz</label>
+          <input ref={inputRef} type={"text"} />
+          <button
+            onClick={() => {
+              setRoom(inputRef.current.value);
+            }}
+          >
+            Odaya Gir
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/firebase/FirebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbGMbp-I_JDyGj0QZgx3VgjceVNM0APA0",
+  authDomain: "chatroom-9803a.firebaseapp.com",
+  projectId: "chatroom-9803a",
+  storageBucket: "chatroom-9803a.appspot.com",
+  messagingSenderId: "655436794697",
+  appId: "1:655436794697:web:926dd66a873244bc926898",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
